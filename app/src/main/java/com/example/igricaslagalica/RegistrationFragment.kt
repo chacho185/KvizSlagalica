f74b3c6b6989ec1800486c4a53f8f2a1c9cde511
stefankalajdzic <62014074+stefankalajdzic@users.noreply.github.com>
2023-06-25 15:07:43 +0200

    Login and registration

---
+++ app/src/main/java/com/example/igricaslagalica/RegistrationFragment.kt
@@ -0,0 +1,54 @@
+package com.example.igricaslagalica
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.igricaslagalica.databinding.FragmentRegistrationBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class RegistrationFragment : Fragment() {
+
+    private var _binding: FragmentRegistrationBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentRegistrationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonRegister.setOnClickListener {
+            registerUser()
+            //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    private fun registerUser() {
+        val email = binding.editTextEmail.text.toString()
+        val username = binding.editTextUsername.text.toString()
+        val password = binding.editTextPassword.text.toString()
+        val confirmPassword = binding.editTextConfirmPassword.text.toString()
+
+        //TODO napraviti logiku za firebase
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
